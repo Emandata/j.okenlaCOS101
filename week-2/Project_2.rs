a9f01e0ac16c10b0551cb03ffbc453521d26ab87
Emandata <eokenla02@gmail.com>
2024-10-26 22:59:40 +0100

    Add files via upload

---
+++ week-2/Project_2.rs
@@ -0,0 +1,14 @@
+fn main() {
+	let toshiba = 2 * 450000;
+	let mac = 1 * 1500000;
+	let hp = 3 * 750000;
+	let dell = 3 * 2850000;
+	let acer = 1 * 250000;
+
+	let sum = acer + dell+ hp + mac + toshiba;
+	println!("sum of sales record is {}", sum );
+
+	let average = sum / 5;
+	println!("the average of sales record is {}",average );
+
+}
---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	 // print just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguements", "some" ); //prints format some arguements
+
+	println!("My name is {}. I am the class rep of stream 1", "Okenla Emmanuel" );
+}		
